eb7bb0c345186de42775db8eaa25a9719198368e
James Tapsell <5932002+jrtapsell@users.noreply.github.com>
2024-07-13 21:26:11 +0100

    Renamed JS lint failing name

--- static/index.js
+++ static/index.js
@@ -1,30 +1,30 @@
-Array(...document.querySelectorAll("img.install_qr")).forEach(elem_image => {
-    elem_image = elem_image.parentElement
-    elem_image.onclick = () => {
-        elem_image.requestFullscreen()
-    }
+[...document.querySelectorAll('img.install_qr')].forEach(imageelement => {
+  imageelement = imageelement.parentElement
+  imageelement.onclick = () => {
+    imageelement.requestFullscreen()
+  }
 
-    elem_image.onfullscreenchange = () => {
-        if (document.fullscreenElement) {
-            console.log("Entered fullscreen", elem_image)
-        } else {
-            console.log("Left fullscreen", elem_image)
-        }
+  imageelement.onfullscreenchange = () => {
+    if (document.fullscreenElement) {
+      console.log('Entered fullscreen', imageelement)
+    } else {
+      console.log('Left fullscreen', imageelement)
     }
+  }
 
-    elem_image.addEventListener("keypress", function(event) {
-        // If the user presses the "Enter" key on the keyboard
-        if (event.key === "Enter") {
-          // Cancel the default action, if needed
-          event.preventDefault();
-          // Trigger the button element with a click
-          elem_image.click();
-        }
-      });
-})
-
-Array(...document.querySelectorAll(".exit_fullscreen")).forEach((p) => {
-    p.onclick = () => {
-        document.exitFullscreen()
+  imageelement.addEventListener('keypress', function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === 'Enter') {
+      // Cancel the default action, if needed
+      event.preventDefault()
+      // Trigger the button element with a click
+      imageelement.click()
     }
+  })
+});
+
+[...document.querySelectorAll('.exit_fullscreen')].forEach((p) => {
+  p.onclick = () => {
+    document.exitFullscreen()
+  }
 })
